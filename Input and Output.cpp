2ff2e1322250a019b0fa23f7e3ce503a00c6f884
shivam7374 <shivamsingla491@gmail.com>
2020-03-17 00:04:14 +0530

    Input and Output

---
+++ Input and Output.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {int s,a,b,c;
+cin>>a>>b>>c;
+s=(a+b+c);
+cout<<s;
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
