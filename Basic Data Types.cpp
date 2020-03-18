407b45dd2d2b3497aad41b0458a8a5730ea2c7e6
shivam7374 <shivamsingla491@gmail.com>
2020-03-18 23:33:51 +0530

    Basic Data Types

---
+++ Basic Data Types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float f;
+    double d;
+    cin>>a>>b>>c>>f>>d;
+    cout<<a<<endl<<b<<endl<<c<<endl<<fixed << setprecision(3)<<f<<endl<<fixed << setprecision(9)<<d;
+    return 0;
+}
+
