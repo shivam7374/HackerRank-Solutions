bbeba2b5208e5f9d3500a9860094875e667464b5
shivam7374 <shivamsingla491@gmail.com>
2020-03-19 17:40:58 +0530

    Inherited Code

---
+++ Inherited Code.cpp
@@ -0,0 +1,15 @@
+
+
+/* Define the exception here */
+class BadLengthException {
+    private: 
+        int n;
+    public:
+        BadLengthException(int errornumber) {
+            n = errornumber;
+        }
+    
+        int what() {
+            return n;
+        }
+};
