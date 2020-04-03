28d050b3853b68a925d7724c67d95a71c5825364
shivam7374 <shivamsingla491@gmail.com>
2020-04-04 00:31:18 +0530

    Rectangle Area

---
+++ Rectangle Area.cpp
@@ -0,0 +1,26 @@
+
+
+class Rectangle
+{public:
+int l,b;
+void display()
+{
+cout<<l<<" "<<b<<endl;
+}
+};
+
+class RectangleArea:public Rectangle
+{
+public:
+void read_input(){
+    cin>>l>>b;
+
+};
+void display()
+{
+    cout<<l*b<<endl;
+}
+};
+
+
+
